39adfaf4a6437fe8f1a95846ee8be9ae7e4ef33b
Roland Levy <contact@rolandlevy.co.uk>
2024-04-03 22:25:48 +0000

    creating image elem dynamically

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const $ = (selector) => document.querySelector(selector);
-const randomUrl = 'https://source.unsplash.com/random?1';
 
 document.addEventListener('DOMContentLoaded', (e) => {
-  $('.image').addEventListener('click', async (e) => {
+  const img = document.createElement('img');
+  img.setAttribute('class', 'image show');
+  img.addEventListener('click', async (e) => {
+    const randomUrl = 'https://source.unsplash.com/random?1';
     const { url } = await fetch(randomUrl);
     e.target.classList.remove('show');
     e.target.addEventListener('transitionend', (e) => {
-      e.target.src = url;
       e.target.classList.add('show');
+      e.target.src = url;
     });
   });
+  $('.photo').appendChild(img);
 
   $('.image').click();
 });
